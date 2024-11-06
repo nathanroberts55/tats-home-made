66a599506f00cc4dc23ed874241fc3c55541ba38
nathanroberts55 <nathan.roberts.15@cnu.edu>
2024-11-05 20:47:21 -0500

    Updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,8 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			// See below for an explanation of these options
 			routes: {
@@ -19,8 +15,7 @@ const config = {
 				persist: false
 			}
 		})
-	},
-	preprocess: vitePreprocess()
+	}
 };
 
 export default config;
